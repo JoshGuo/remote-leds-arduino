78eefb3699934fab9eecf5bfcba4b1b7fde4981f
Josh Guo <joshua.guo@stonybrook.edu>
2021-09-19 21:27:47 -0400

    Warm fade setting and raise brightness

--- main/main.ino
+++ main/main.ino
@@ -1,38 +1,30 @@
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <FastLED.h>
 #include <ArduinoJson.h>
-#include <WiFiClientSecureBearSSL.h>
+#include "enums.h"
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <NeoPixelBus.h>
 #include <pt.h>
+#include <WiFiClientSecureBearSSL.h>
 
-#define WIFI_SSID "f231fa"
-#define WIFI_PASS "gain.037.barrier"
+#define WIFI_SSID "WolfieNet-IoT"
+#define WIFI_PASS NULL
+#define REQUEST_INTERVAL 5000
+#define MINS_TO_SLEEP 5 
+
+#define BRIGHTNESS 100
+#define LIGHTNESS .34f
 #define NUM_LEDS 300
-#define BRIGHTNESS 75
-#define LED_PIN 5
-
-enum LED_MODE {
-  OFF = -1,
-  SOLID = 0,
-  RAINBOW = 1,
-  FLASH = 2
-};
+#define LED_PIN 4
+#define DEFAULT_LED_MODE FADE
 
 // LED Vars
-int led_hues[NUM_LEDS];
-CRGB leds[NUM_LEDS];
-LED_MODE ledMode = OFF;
-long timer;
-bool isOn = false;
-CRGB color;
-int r, g, b;
+NeoPixelBus<NeoGrbFeature, Neo800KbpsMethod> strip(NUM_LEDS);
+LED_MODE ledMode = DEFAULT_LED_MODE;
 
-long pressTime;
-long deltaTimeButton = 0;
+const String API = "https://remote-leds.herokuapp.com/queue";
 
 // Timer Vars
-long getInitTime;
-long deltaLEDTime;
+long lastRequestTime;
 
 pt ptRequest;
 
@@ -41,30 +33,47 @@ std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure)
 HTTPClient http;
 StaticJsonDocument<256> doc; //For Json Data Extraction
 
-/**
- * Setup leds
- */
-void initLeds() {
-  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.clear(true);
-  FastLED.setBrightness(BRIGHTNESS);
+void httpRequest(String url) {
+  Serial.print("Requesting @ " + url + "...");
+  http.begin(*client, url);
+  int httpCode;
+  httpCode = http.GET();
+  Serial.println(httpCode);
+
+  if (httpCode == 200) {
+    deserializeJson(doc, http.getString());
+    ledMode = (LED_MODE) doc["mode"];
+    Serial.println(ledMode);
+    initLed();
+  }else if(httpCode == 400) {
+    Serial.println("No effect queued");
+  }
+  
+  lastRequestTime = millis();
+  http.end();
+}
+
+int httpRequestThread(struct pt* pt) {
+  PT_BEGIN(pt);
+  while(1) {
+    PT_WAIT_UNTIL(pt, millis() - lastRequestTime > REQUEST_INTERVAL);
+    httpRequest(API + "/dequeue");
+  }
+  PT_END(pt);
 }
 
 /**
- * Flash leds for testing
+ * Flash through red green and blue
  */
 void ledTestCycle() {
-  delay(500);
-  CRGB colors[] = {CRGB::Red, CRGB::Green, CRGB::Blue};
-  for(CRGB color : colors) {
+  RgbColor colors[] = {RgbColor(BRIGHTNESS,0,0), RgbColor(0,BRIGHTNESS,0), RgbColor(0,0,BRIGHTNESS), RgbColor(0,0,0)};
+  for(RgbColor color : colors) {
     for(int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = color;
+      strip.SetPixelColor(i, color);
     }
-    FastLED.show();
-    delay(500);
+    strip.Show();
+    delay(1000);
   }
-  FastLED.clear(true);
-  delay(500);
 }
 
 /**
@@ -81,210 +90,145 @@ void connectToWifi() {
     digitalWrite(LED_BUILTIN, ledVal);
     delay(1000);
   }
-  Serial.println("Connected!");
-
-  //Once connected show LED
-  digitalWrite(LED_BUILTIN, LOW);
   Serial.println("\nConnected to WiFi!");
   Serial.println(WiFi.localIP()); 
 }
 
-int httpRequestThread(struct pt* pt) {
-  PT_BEGIN(pt);
-  while(1) {
-    PT_WAIT_UNTIL(pt, millis() - getInitTime > 7500);
-    httpRequest("https://remote-leds.herokuapp.com/queue/dequeue");
+void turnOff() {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    RgbColor black(0, 0, 0);
+    strip.SetPixelColor(i, black);
   }
-  
-
-  PT_END(pt);
+  strip.Show();
 }
 
-/**
- * Change the mode of the lds 
- */
-void changeMode(int newMode) {
-  if(newMode == 1 && ledMode == 1) return;
+void setColor(RgbColor color) {
+  color.Darken(50);
+  for(int i = 0; i < NUM_LEDS; i++) {
+    strip.SetPixelColor(i, color);
+  }
+  strip.Show();
+}
 
-  ledMode = (LED_MODE)newMode;
+void initSmoothFadeBands(int startHue, int finalHue, int repeats) {
+  if(repeats > 10 || repeats <= 0 || NUM_LEDS % repeats != 0) repeats = 1;
+
+  int bandLength = NUM_LEDS / repeats;
+  for(int cycle = 0; cycle < repeats; cycle++) {
+    for(int led = 0; led < bandLength / 2; led++) {
+      int hue; 
+      if(startHue <= finalHue) {
+        hue = (float) led / (bandLength / 2) * (finalHue - startHue) + startHue;
+      }else {
+        hue = (float) led / (bandLength / 2) * (finalHue + 360 - startHue) + startHue;
+      }
+      hue = hue % 360;
+      strip.SetPixelColor(cycle * bandLength + led, HsbColor((float) hue / 360, 1, LIGHTNESS));
+      strip.SetPixelColor((cycle + 1) * bandLength - led - 1, HsbColor((float) hue / 360, 1, LIGHTNESS));
+    }
+  }
+  strip.Show();
+}
+
+void initFullFade() {
+  initSmoothFadeBands(0, 359, 1);
+}
+
+void initCoolFade() {
+  initSmoothFadeBands(170, 290, 2);
+}
+
+void initWarmFade() {
+  initSmoothFadeBands(340, 45, 2);
+}
+
+void initLed() {
+  switch(ledMode) {
+    case OFF: {
+      turnOff();
+    }
+      break;
+    case SOLID: {
+      // Extract color from JSON doc
+      HtmlColor color;
+      const char* colorString = doc["color"];
+      color.Parse<HtmlColorNames>(colorString);
+      setColor(RgbColor(color));
+    }
+      break;
+    case FADE: {
+      FADE_TYPE fadeType = (FADE_TYPE) doc["type"];
+      if(fadeType == NULL) fadeType = FULL;
+      switch(fadeType) {
+        case FULL: 
+          initFullFade();
+          break;
+        case COOL:
+          initCoolFade();
+          break;
+        case WARM:
+          initWarmFade();
+          break;
+        case CUSTOM:
+        default:
+          break;
+      }
+    }
+    default: 
+      break;
+  }
+}
+
+void doLed() {
+  static long flashTime = 0;
   switch (ledMode) {
-    case OFF:
-      FastLED.clear(true);
+    case FADE: {
+      strip.RotateLeft(1);
+      strip.Show();
+      delay(35);
+    }
       break;
-    case SOLID:
-      FastLED.setBrightness(BRIGHTNESS);
-      extractColorFromJson();
-      initLEDColor();
-      break;
-    case RAINBOW: 
-      FastLED.setBrightness(BRIGHTNESS);
-      initFade();
-      break;
-    case FLASH: 
-      FastLED.setBrightness(BRIGHTNESS);
-      extractColorFromJson();
-      initFlash();
-      break;
-    default: break;
-  }
-}
+    case FLASH: {
+      if(millis() - flashTime > 2000) {
 
-/**
- * Turn off the LEDs (set all to black)
- */
-void turnOff(int first, int last) {
-  for (int i = first; i < last; i++) {
-    leds[i].setHSV(255, 255, 255);
+      }
+    }
+    default: break; //Other settings do not require continuous updates
   }
-  FastLED.show();
 }
 
 void setup() {
   // Init Serial
   Serial.begin(9600);
-  while(!Serial) delay(250);
-  Serial.println("");
+  while(!Serial)
   Serial.println("Program Started");
   Serial.flush();
-
-  //Enable flash to be used as a toggle button.
-  // pinMode(0, INPUT_PULLUP);
-  // buttonIsPressed = false;
-
+  
   // Init built-in led
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
-  
-  // Init leds
-  initLeds();
-  ledTestCycle();
 
-  // Connect to Wifi and perform setup
-  connectToWifi();
+  // Set up wifi
+  // connectToWifi();
+  bool ledIsOn = true;
+  while (WiFi.status() != WL_CONNECTED) {
+    digitalWrite(LED_BUILTIN, ledIsOn);
+    ledIsOn = !ledIsOn;
+    delay(500);
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
   client->setInsecure();
 
-  // Initial http request
-  httpRequest("https://remote-leds.herokuapp.com/queue/dequeue");               
+  // Init leds
+  strip.Begin();
+  strip.Show();
+  ledTestCycle();
 
+  httpRequest(API + "/current");           
   PT_INIT(&ptRequest);                                                                                                                                                                                                                                                                                           
 }
 
-/**
- * Handler for special LED logic every cycle
- */
-void doLED() {
-  switch (ledMode) {
-    case RAINBOW: fade(); //If fade, move leds along. (maybe change this to a less rainbow-y and more gradual fade)
-      break;
-    case FLASH: flash();
-    default: break; //Other settings do not require continuous updates
-  }
-}
-
-
 void loop() {
-  // GET latest led config every 10 seconds
-  // Would be better to update this to a socket stream in the future
   PT_SCHEDULE(httpRequestThread(&ptRequest));
-  doLED();
-}
-
-void extractColorFromJson() {
-  const char* hex = doc["color"];
-  char colorBuf[5];
-  colorBuf[0] = '0';
-  colorBuf[1] = 'x';
-
-  colorBuf[2] = hex[1];
-  colorBuf[3] = hex[2];
-  color.r = strtol(colorBuf, NULL, 16); // Red
-
-  colorBuf[2] = hex[3];
-  colorBuf[3] = hex[4];
-  color.g = strtol(colorBuf, NULL, 16); // Green
-
-  colorBuf[2] = hex[5];
-  colorBuf[3] = hex[6];
-  color.b = strtol(colorBuf, NULL, 16); // Blue
-}
-
-void initLEDColor() {
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = color;
-  }
-  FastLED.show();
-}
-
-/**
- * Init ~1/3 of the color spectrum across all 300 leds
- */
-void initFade() {
-  // Serial.println("init fade");
-  for(int hue = 0; hue < 100; hue++) {
-    for(int led = 0; led < 3; led++) {
-      leds[hue * 3 + led].setHue(hue);
-      led_hues[hue * 3 + led] = hue;
-    }
-  }
-  FastLED.show();
-}
-
-void fade() {
-  for(int led = 0; led < NUM_LEDS; led++) {
-    led_hues[led] = (led_hues[led] + 1) % 256;
-    leds[led].setHue(led_hues[led]);
-  }
-  delay(30);
-  FastLED.show();
-}
-void initFlash() {
-  isOn = true;
-  timer = millis();
-  initLEDColor();
-}
-
-void moveLED() {
-  CRGB temp = leds[299];
-  for (int count = NUM_LEDS - 1; count > 0; count--) {
-    leds[count] = leds[count - 1];
-  }
-  leds[0] = temp;
-  delay(50); // Might need to process this using delta Time? Not sure if this would cause problems with reading from serial input
-  FastLED.show();
-}
-
-void flash() {
-  long currTime = millis();
-  bool switched = false;
-  if (currTime - timer > 1000) {
-    isOn = !isOn;
-    timer = currTime;
-    switched = true;
-  }
-  if (switched) {
-    if (isOn) {
-      initLEDColor();
-    } else {
-      FastLED.clear(true);
-    }
-  }
-}
-
-void httpRequest(String url) { //Later change the GET request to occur within a while loop with http.connected to simulate a async function();
-  // Serial.print("Requesting @ " + url + "...");
-  http.begin(*client, url);
-  int httpCode;
-  httpCode = http.GET();
-  // Serial.println(httpCode);
-
-  if (httpCode == 200) {
-    deserializeJson(doc, http.getString());
-    const int modeNum = doc["mode"];
-    changeMode(modeNum);
-  }
-  
-  getInitTime = millis();
-  //Dunno if i really need to support post request
-  http.end();
-
+  doLed();
 }
